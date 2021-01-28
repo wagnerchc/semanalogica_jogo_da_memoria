7c886e043cc1b82148e771f1610ca94f1d3a0edb
Wagner Chequeleiro Cordeiro <w.chequeleiro@gmail.com>
2021-01-27 21:53:16 -0300

    aula2

--- app.js
+++ app.js
@@ -9,33 +9,39 @@ const board = document.querySelector(".table_board");
 
 let pairs = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 
+let selectedCards = []
+
 pairs.forEach(pair => {
     const card = document.createElement("div");
-
     card.className = "table_board_card";
+    card.dataset.value = pair;
     card.innerText = pair;
-
     card.addEventListener("click", itIsAMatch);
-    
     board.append(card);
 });
 
-let value1
-let value2
-
-function itIsAMatch(card) {
-    if(value1) {
-        value2 = card.target.innerText;
-    } else {
-        value1 = card.target.innerText;
+function itIsAMatch(value) {
+    const clickedCard = value.target;
+    if(clickedCard.classList.contains('open')) {
+        return
     }
-    if(value1 == value2) {
-        console.log("Formou um par")
-    } else {
-        console.log("Continue tentando")
+    clickedCard.classList.add('open');
+    selectedCards.push(clickedCard)
+    if(selectedCards.length > 1) {
+        const [firstCard, secondCard] = [...selectedCards];
+        if(firstCard.dataset.value === secondCard.dataset.value) {
+            alert("São pares")
+            firstCard.classList.remove('open')
+            firstCard.classList.add('pairform')
+            secondCard.classList.remove('open') 
+            secondCard.classList.add('pairform')
+            // colocar um marcador, para não entrar mais array
+        } else if (firstCard.dataset.value !== secondCard.dataset.value) {
+        }
+        setTimeout(() => {
+            firstCard.classList.remove('open')
+            secondCard.classList.remove('open')   
+        }, 800); //milisegundos 1000ms = 1s
+        selectedCards = []    
     }
-    
-    console.log("O primeiro valor é: ", value1);
-    console.log("O segundo valor é: ", value2);
-    
 }
